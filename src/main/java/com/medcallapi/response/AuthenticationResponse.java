46ab9ad9098d60fa799db6850cfe4941b51b0a6a
BOUZIDI Med Ali <med23908@gmail.com>
2021-09-29 19:22:15 +0100

    Update: Customize Authentication response

--- src/main/java/com/medcallapi/response/AuthenticationResponse.java
+++ src/main/java/com/medcallapi/response/AuthenticationResponse.java
@@ -1,14 +1,18 @@
 package com.medcallapi.response;
 
 import com.medcallapi.entity.UserEntity;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@AllArgsConstructor
+
 @Getter
 public class AuthenticationResponse {
 
     private final UserEntity user;
     private final String jwt;
 
+    public AuthenticationResponse(UserEntity user, String jwt) {
+        this.user = user;
+        this.user.setPassword("");
+        this.jwt = jwt;
+    }
 }
