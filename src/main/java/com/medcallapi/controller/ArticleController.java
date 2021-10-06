2eef37345db313ba4d119d6213d21b3471647e0b
BOUZIDI Med Ali <med23908@gmail.com>
2021-10-06 20:05:14 +0100

    Add: return article by id

--- src/main/java/com/medcallapi/controller/ArticleController.java
+++ src/main/java/com/medcallapi/controller/ArticleController.java
@@ -1,14 +1,14 @@
 package com.medcallapi.controller;
 
+import com.medcallapi.entity.Article;
 import com.medcallapi.response.ArticleResponse;
 import com.medcallapi.service.ArticleService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @AllArgsConstructor
 @RestController
@@ -18,9 +18,20 @@ public class ArticleController {
 
     private ArticleService articleService;
 
-    @GetMapping(path = "/article")
+    @GetMapping(path = "/articles")
     public List<ArticleResponse> index() {
         return articleService.index();
     }
+
+    @GetMapping(path = "/articles/{id}")
+    public ResponseEntity<Optional<Article>> show(@PathVariable Long id) {
+        return articleService.show(id);
+    }
+
+    // Add New Article
+
+    // Update Article
+
+    // Delete Article
 
 }
--- src/main/java/com/medcallapi/service/ArticleService.java
+++ src/main/java/com/medcallapi/service/ArticleService.java
@@ -4,10 +4,13 @@ import com.medcallapi.entity.Article;
 import com.medcallapi.repository.ArticleRepository;
 import com.medcallapi.response.ArticleResponse;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @AllArgsConstructor
 @Service
@@ -17,10 +20,14 @@ public class ArticleService {
     public List<ArticleResponse> index() {
         List<Article> articles = articleRepository.findAll();
         List<ArticleResponse> articlesResponse = new ArrayList<>();
-        for (Article article: articles) {
+        for (Article article : articles) {
             articlesResponse.add(new ArticleResponse(article));
         }
         return articlesResponse;
     }
 
+    public ResponseEntity<Optional<Article>> show(Long id) {
+        Optional<Article> article = articleRepository.findById(id);
+        return article.isPresent() ? ResponseEntity.status(HttpStatus.FOUND).body(article) : ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
 }
